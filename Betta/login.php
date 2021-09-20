91e821ac00e7aa7152f13f51863283eed401b813
CAROLINE NATHALY DA LUZ DA COSTA <caroline.costa3@alunos.sc.senac.br>
2021-09-20 15:04:55 -0300

    mini

--- Betta/login.php
+++ Betta/login.php
@@ -12,16 +12,16 @@ include_once(dirname(__FILE__) . "/admin/functions/login.usuario.php");
 </div>
 <form method="post" >
     <div class="form-row suco">
-        <div class="form-group col-md-6">
+        <div class="form-group col-6 meio">
             <label>Email</label>
             <input type="email" name="email" class="form-control" placeholder="Email">
         </div>
-        <div class="form-group col-md-6">
+        <div class="form-group col-6 meio">
             <label>Senha</label>
             <input type="password" name="senha" class="form-control" placeholder="Senha">
         </div>
     </div>
-    <div class="login ra">
+    <div class="login meio">
         <input type="submit" class="btn btn-warning" name="login" value="Entrar"></button>
         <input type="submit" class="btn btn-warning" name="cadastro" value="Cadastro"><a href="cadastro.php"></a></button>
     </div>
